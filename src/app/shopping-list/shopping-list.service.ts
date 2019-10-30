9c9933a3db6d106d10c8d5f11fade81a8a5c55c5
shivakumarsuryan@gmail.com <shivakumarsuryan@gmail.com>
2019-10-30 12:40:56 +0530

    Adding ingredients to Shopping-List from Recipe-Detail page.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -5,10 +5,12 @@ export class ShoppingListService {
 
     updateShoppingListEvent = new EventEmitter<Ingredient[]>();
 
-    private ingredients: Ingredient[] = [
-        new Ingredient('apple', 5),
-        new Ingredient('tomatoes', 5),
-    ];
+    private ingredients: Ingredient[] = [];
+
+    // private ingredients: Ingredient[] = [
+    //     new Ingredient('apple', 5),
+    //     new Ingredient('tomatoes', 5),
+    // ];
 
     getIngredients() {
         return this.ingredients.slice();
@@ -16,6 +18,11 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
+        this.updateShoppingListEvent.emit(this.ingredients.slice());
+    }
+
+    addIngredients(ingredientsUp: Ingredient[]) {
+        this.ingredients = this.ingredients.concat(ingredientsUp);
         this.updateShoppingListEvent.emit(this.ingredients.slice());
     }
 }
